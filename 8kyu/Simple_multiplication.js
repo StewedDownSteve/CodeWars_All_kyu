164db34ad94805f1d7c886a543ac68a30b2daa13
Steve Rep <steven.replogle1@gmail.com>
2023-09-16 11:01:36 -0700

    daily kyu challenge

---
+++ 8kyu/Simple_multiplication.js
@@ -0,0 +1,13 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+function simpleMultiplication(number) {
+    if (number % 2 === 0) {
+        return (number * 8)
+    } else {
+        return (number * 9)
+    }
+}
+
+
+// return n * (n % 2 ? 9 : 8);
