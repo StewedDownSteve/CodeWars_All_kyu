43dc3ca23dd85f3b5f4b2418b32170c60fa7e549
Steve Rep <steven.replogle1@gmail.com>
2023-10-01 09:48:34 -0700

    daily codewars challenge

---
+++ 8kyu/Bin_to_Decimal.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+    return parseInt(bin, 2)
+  }
+
+
+//   parseInt() takes a string, puts a radix next to it
