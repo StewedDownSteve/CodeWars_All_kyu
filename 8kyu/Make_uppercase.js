68efbedbc04da35a301153f01daf88f4e5bec618
Steve Rep <steven.replogle1@gmail.com>
2023-10-04 09:29:38 -0700

    daily coding challenge

---
+++ 8kyu/Make_uppercase.js
@@ -0,0 +1,10 @@
+// Write a function which converts the input string to uppercase.
+
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
+
+//   OR in codewars style...
+
+const makeUpperCase1 = str1 => str1.toUpperCase();
