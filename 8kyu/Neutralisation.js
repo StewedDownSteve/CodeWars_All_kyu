f6a403140afe13938ddaa488d34541431b0eccee
Steve Rep <steven.replogle1@gmail.com>
2023-10-20 09:35:46 -0700

    finally finished this one

--- 8kyu/Neutralisation.js
+++ 8kyu/Neutralisation.js
@@ -20,6 +20,36 @@
 // The two strings will be the same length.
 
 function neutralise(s1, s2) {
-    // Here be dragons!
-    return "";
+    let result = "";
+    for (let i = 0; i < s1.length; i++) {
+      const char1 = s1[i];
+      const char2 = s2[i];
+      if (char1 === "+" && char2 === "+") {
+        result += "+";
+      } else if (char1 === "-" && char2 === "-") {
+        result += "-";
+      } else {
+        result += "0";
+      }
+    }
+    return result;
+  }
+
+  // make an empty var to hold the result of the new string
+  // make a for loop to go through the cahracters of both strings
+  // create a var for each string, that holds i from the loop
+  // compare each character to the character in the other string, using the stated rules
+  // positives and positives remain positive, += to result, new var
+  // neg and nega remain neg, += to result
+  // else, neg and pos, the only other option, result += 0
+  // return result
+
+
+  // shorter version below
+  function neutralise1(str1, str2) {
+    let res = "";
+    for(let i = 0; i < s1.length; i++) {
+      res += str1[i] !== str2[i]? "0": str1[i];
+    }
+    return res;
   }
