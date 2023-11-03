404980975226d76fa4c3afe92fa87aa6440f29e2
Steve Rep <steven.replogle1@gmail.com>
2023-11-03 11:22:52 -0700

    daily coding challenge

--- 8kyu/How_good_are_you_really.js
+++ 8kyu/How_good_are_you_really.js
@@ -2,7 +2,6 @@
 // But you're an ambitious person. You want to know if you're better than the average student in your class.
 
 // You receive an array with your peers' test scores. Now calculate the average and compare your score!
-
 // Return True if you're better, else False!
 // Note:
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array! -->
