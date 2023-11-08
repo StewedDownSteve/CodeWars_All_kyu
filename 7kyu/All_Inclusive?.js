c8fc5fee28519cc9ca445860df09c5115c8a2df2
Steve Rep <steven.replogle1@gmail.com>
2023-11-08 09:39:48 -0800

    daily coding challenge 7kyu

---
+++ 7kyu/All_Inclusive?.js
@@ -0,0 +1,33 @@
+// nput:
+
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+// Note:
+// Though not correct in a mathematical sense
+
+// we will consider that there are no rotations of strng == ""
+// and for any array arr: contain_all_rots("", arr) --> true
+
+function containAllRots(strng, arr) {
+    for(let i = 0; i < strng.length; i++) {
+        if (arr.indexOf(strng.slice(i) + strng.slice(0,i)) == -1) {
+            return false
+        }
+    }
+    return true
+}
+
+// make a for loop to go through strng
+// if, indexOf allows us to search arr, we are looking for strng.slice(i), that goes through each index of strng
+// if it is == -1 it means that its not there, so return false
+// otherwise return true
