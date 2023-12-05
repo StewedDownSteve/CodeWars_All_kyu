7050db76f316ad92eee204940b144c9cec89f11a
Steve Rep <steven.replogle1@gmail.com>
2023-12-05 10:42:25 -0800

    daily coding challenge

--- 7kyu/Vowel_Count.js
+++ 7kyu/Vowel_Count.js
@@ -18,18 +18,3 @@ function getCount(str) {
 }
 
 
-// Loop Method
-
-function getCount1(str1) {
-    let vowelsCount = 0;
-    let vowels = ["a","e","i","o","u"];
-    for(let i = 0;i < str1.length;i++){
-      for(let j=0;j<vowels.length;j++){
-        if(str1[i] === vowels[j]){
-          vowelsCount++;
-        }
-      }
-    }
-    
-    return vowelsCount;
-  }
