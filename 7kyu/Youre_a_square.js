beb9726bc9bf67fa070212217eb379c1328d34a1
Steve Rep <steven.replogle1@gmail.com>
2023-12-19 10:11:50 -0800

    daily coding challenge

--- 7kyu/Youre_a_square.js
+++ 7kyu/Youre_a_square.js
@@ -2,7 +2,6 @@
 // You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 
 // However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
-
 // Task
 // Given an integral number, determine if it's a square number:
 
