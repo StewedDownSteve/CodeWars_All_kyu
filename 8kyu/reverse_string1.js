c0bc3ea4d57641fd0017f236dbd19cb7cb989c78
Steve Rep <steven.replogle1@gmail.com>
2024-01-09 14:27:37 -0800

    daily coding challenge

---
+++ 8kyu/reverse_string1.js
@@ -0,0 +1,6 @@
+// Given a string of text, write an algorithm that returns the text received in a reversed format. 
+// E.g reverseString('algorithms') // should return 'smhtirogla'
+// 
+function reverseString(text) {
+    return text.split("").reverse().join("")
+}
