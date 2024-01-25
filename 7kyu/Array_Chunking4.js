6f0d02ef1f5021abc88269ac4980077e7f8404eb
Steve Rep <steven.replogle1@gmail.com>
2024-01-25 14:57:11 -0800

    daily JS coding algo and data

---
+++ 7kyu/Array_Chunking4.js
@@ -0,0 +1,11 @@
+// Given an array and a chunk size, divide the array into many sub-arrays where each sub-array has the specified length. E.g
+
+chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
+// should return [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]
+
+function chunkArray(array, size) {
+    if(array.length <= size){
+        return [array]
+    }
+    return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
+}
