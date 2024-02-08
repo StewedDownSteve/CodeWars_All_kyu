f489cbadbc67c0c5775b4f9a1195b6117531fca4
Steve Rep <steven.replogle1@gmail.com>
2024-02-08 15:32:49 -0800

    new lyu folder

---
+++ 5kyu/Calculating_with_functions2.js
@@ -0,0 +1,15 @@
+const zero = a => a ? a(0) : 0
+const one = a => a ? a(1) : 1
+const two = a => a ? a(2) : 2
+const three = a => a ? a(3) : 3
+const four = a => a ? a(4) : 4
+const five = a => a ? a(5) : 5
+const six = a => a ? a(6) : 6
+const seven = a => a ? a(7) : 7
+const eight = a => a ? a(8) : 8
+const nine = a => a ? a(9) : 9
+
+const plus = a => b => a + b
+const minus = a => b => b - a
+const dividedBy = a => b => Math.floor(b / a)
+const times = a => b => a * b
