fdfdde4ca8851ee51da1f2c2186c33b8a3831584
Steve Replogle <steverep@Steves-MacBook-Pro.local>
2024-03-27 16:57:56 -0700

    daily coding challenge

---
+++ 8kyu/Remove_String_spaces.js
@@ -0,0 +1,25 @@
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// Examples:
+
+// Input -> Output
+// "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+// "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+// "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+
+
+
+
+
+
+
+function noSpace(x){
+    let result = x.replace(/\s/g,'');
+  return result;
+}
+
+// replace() only replaces the first instances 
+// using regular expression to take out the space globaly
+// \s is used to represent spaces
+// /g makes it global, then replaces it with nothing
