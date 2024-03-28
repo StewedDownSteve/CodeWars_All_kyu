dac6cce2bb6eca25066e31e8d305fd71a6159b81
Steve Replogle <steverep@Steves-MacBook-Pro.local>
2024-03-28 13:00:35 -0700

    daily coding challenge JS

---
+++ 7kyu/Round_up_to_the_next_mutiple_of5.js
@@ -0,0 +1,25 @@
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+
+function roundToNext5(n){
+    // if the n is div by 5 then return number
+    if ( n % 5 === 0){
+        return n;
+    } else {
+        // math.ceil round ups fractional numbers
+        //  number div by 5, round up, times by 5
+        return Math.ceil( n / 5) * 5
+    }
+]
