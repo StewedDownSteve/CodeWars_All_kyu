12bbe89f00f8741ae68804a07d424e83bf98f501
Steve R. <steverepdev@gmail.com>
2024-04-10 12:26:29 -0700

    daily coding challenge

---
+++ 5kyu/String_incremeneter2.js
@@ -0,0 +1,12 @@
+function incrementString(input) {
+    if(isNaN(parseInt(input[input.length - 1]))) return input + '1';
+    return input.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+      var up = parseInt(p2) + 1;
+      return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+    });
+  }
+
+
+//   the first line checks if the last index of the string is a number
+// Usiing isNan to see if its not a number and parseInt to conver all of itnto a string
+// 
